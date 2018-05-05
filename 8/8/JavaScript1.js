87aea087cd385935c918f506550527cf00bbf591
Azadeh-PC\Azadeh <azadeheshaghii@gmail.com>
2018-05-05 12:25:20 +0430

    no message

---
+++ 8/8/JavaScript1.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    var $listitems = $('.btn-group button');
+    $listitems.click(function () {
+        $listitems.removeClass('active');
+
+        $(this).addClass('active');
+
+    });
+    $listitems.mouseover(function () {
+        $listitems.show();
+
+
+
+    });
+
+
+});
---
+++ 8/8/plugin.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="jquery-3.3.1.js" />
+
+(function ($) {
+    $.fn.extend({
+        //plugin name - animatemenu
+        animateMenu: function (options) {
+            debugger;
+            var defaults = {
+                animatePadding: 60,
+                defaultPadding: 10,
+                evenColor: '#ccc',
+                oddColor: '#eee',
+                textColor: '#EC407A',
+            };
+
+            var options = $.extend(defaults, options);
+
+            return this.each(function () {
+                debugger;
+                var o = options;
+                var obj = $(this);
+                var items = $("li", obj);
+
+                $("li:even", obj).css('background-color', o.evenColor);
+                $("li:odd", obj).css('background-color', o.oddColor);
+                $("li", obj).css('color', o.textColor);
+                items.mouseover(function () {
+                    $(this).animate({ paddingLeft: o.animatePadding }, 300);
+                  
+                    });
+                   items .mouseout(function () {
+                    $(this).animate({ paddingLeft: o.defaultPadding }, 300);
+
+                   });
+                //نشان  دادن دیو مخصوص به تب دوم
+                $("li:eq(1)").mouseover(function () {
+                    //$(this).click(function () {
+                        $(".k").show();
+
+                    //});
+                });
+
+                //نشان دادن دیو مخصوص به تب اول
+                $("li:eq(0)").mouseover(function () {
+                    //$(this).click(function () {
+                    $(".k1").show();
+
+                    //});
+                });
+            });
+        }
+    });
+})(jQuery);
+
